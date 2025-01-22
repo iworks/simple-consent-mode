044cb452bdd7c07b0e51d32702eb805ff8580aaa
Marcin Pietrzak <marcin@iworks.pl>
2025-01-22 09:11:48 +0100

    i18n

--- includes/iworks/class-simple-consent-mode.php
+++ includes/iworks/class-simple-consent-mode.php
@@ -32,21 +32,11 @@ class iworks_simple_consent_mode extends iworks_simple_consent_mode_base {
 	public function __construct() {
 		parent::__construct();
 		$this->version    = 'PLUGIN_VERSION';
-		$this->capability = apply_filters( 'iworks_simple_consent_mode_capability', 'manage_options' );
 		/**
-		 * post types
-		 */
-		include_once 'class-simple-consent-mode-posttypes.php';
-		new iworks_wordpress_plugin_posttypes();
-
-		/**
-		 * init
-		 */
-		add_action( 'init', array( $this, 'action_init_load_plugin_textdomain' ), 0 );
-		/**
-		 * admin init
+		 * WordPress Hooks
 		 */
 		add_action( 'admin_init', array( $this, 'action_admin_init' ) );
+		add_action( 'wp_head', array( $this, 'action_wp_head_add_defaults' ), 0 );
 		/**
 		 * is active?
 		 */
@@ -55,93 +45,33 @@ class iworks_simple_consent_mode extends iworks_simple_consent_mode_base {
 
 	public function action_admin_init() {
 		iworks_simple_consent_mode_options_init();
-		add_action( 'admin_enqueue_scripts', array( $this, 'admin_enqueue_scripts' ) );
 		add_filter( 'plugin_row_meta', array( $this, 'plugin_row_meta' ), 10, 2 );
 	}
 
-	public function admin_enqueue_scripts() {
-		$screen = get_current_screen();
-		/**
-		 * off on not simple-consent-mode pages
-		 */
-		$re = sprintf( '/%s_/', __CLASS__ );
-		if ( ! preg_match( $re, $screen->id ) ) {
-			return;
-		}
-		/**
-		 * datepicker
-		 */
-		$file = 'assets/externals/datepicker/css/jquery-ui-datepicker.css';
-		$file = plugins_url( $file, $this->base );
-		wp_register_style( 'jquery-ui-datepicker', $file, false, '1.12.1' );
-		/**
-		 * select2
-		 */
-		$file = 'assets/externals/select2/css/select2.min.css';
-		$file = plugins_url( $file, $this->base );
-		wp_register_style( 'select2', $file, false, '4.0.3' );
-		/**
-		 * Admin styles
-		 */
-		$file    = sprintf( '/assets/styles/admin%s.css', $this->dev );
-		$version = $this->get_version( $file );
-		$file    = plugins_url( $file, $this->base );
-		wp_register_style( 'admin-simple-consent-mode', $file, array( 'jquery-ui-datepicker', 'select2' ), $version );
-		wp_enqueue_style( 'admin-simple-consent-mode' );
-		/**
-		 * select2
-		 */
-		wp_register_script( 'select2', plugins_url( 'assets/externals/select2/js/select2.full.min.js', $this->base ), array(), '4.0.3' );
-		/**
-		 * Admin scripts
-		 */
-		$files = array(
-			'simple-consent-mode-admin' => sprintf( 'assets/scripts/admin/admin%s.js', $this->dev ),
+	public function action_wp_head_add_defaults() {
+		// Set default consent to 'denied' as a placeholder
+		// Determine actual values based on your own requirements
+		$settings = array(
+			'ad_storage'=> 'denied',
+			'ad_user_data'=> 'denied',
+			'ad_personalization'=> 'denied',
+			'analytics_storage'=> 'denied',
+			'functionality_storage' => 'denied',
+			'personalization_storage' => 'denied',
+			'security_storage'=>'denied',
 		);
-		if ( '' == $this->dev ) {
-			$files = array(
-				'simple-consent-mode-admin-datepicker' => 'assets/scripts/admin/src/datepicker.js',
-				'simple-consent-mode-admin-select2'    => 'assets/scripts/admin/src/select2.js',
-				'simple-consent-mode-admin-media-library'    => 'assets/scripts/admin/src/media-library.js',
-			);
-		}
-		$deps = array(
-			'jquery-ui-datepicker',
-			'select2',
+		printf(
+			'<script data-name="%s" data-version="%s">',
+			esc_attr( __CLASS__ ),
+			esc_attr( 'PLUGIN_VERSION' )
 		);
-		foreach ( $files as $handle => $file ) {
-			wp_register_script(
-				$handle,
-				plugins_url( $file, $this->base ),
-				$deps,
-				$this->get_version(),
-				true
-			);
-			wp_enqueue_script( $handle );
-		}
-		/**
-		 * JavaScript messages
-		 *
-		 * @since 1.0.0
-		 */
-		$data = array(
-			'messages' => array(),
-			'nonces'   => array(),
-			'user_id'  => get_current_user_id(),
-		);
-		wp_localize_script(
-			'simple_consent_mode_admin',
-			__CLASS__,
-			apply_filters( 'wp_localize_script_simple_consent_mode_admin', $data )
-		);
-	}
-
-	public function init() {
-		if ( is_admin() ) {
-		} else {
-			$file = 'assets/styles/simple_consent_mode' . $this->dev . '.css';
-			wp_enqueue_style( 'simple-consent-mode', plugins_url( $file, $this->base ), array(), $this->get_version( $file ) );
-		}
+		// Define dataLayer and the gtag function.
+		echo 'window.dataLayer = window.dataLayer || [];';
+		echo 'function gtag(){dataLayer.push(arguments);}';
+		echo 'gtag(\'consent\', \'default\',';
+		echo json_encode( $settings );
+		echo ');';
+		echo '</script>';
 	}
 
 	/**
