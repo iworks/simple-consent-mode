47731aa6edb43fef41f6a56586101dec176766ee
Marcin Pietrzak <marcin@iworks.pl>
2025-02-19 17:41:04 +0100

    prepare new version

--- assets/scripts/simple-consent-mode-frontend.js
+++ assets/scripts/simple-consent-mode-frontend.js
@@ -1,4 +1,4 @@
-/*! Simple Consent Mode - 1.0.0
+/*! Simple Consent Mode - 1.1.0
  * http://simple-consent-mode.iworks.pl/
  * Copyright (c) 2025; * Licensed GPL-3.0 */
 window.simple_consent_mode = window.simple_consent_mode || [];
