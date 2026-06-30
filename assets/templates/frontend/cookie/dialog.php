13ce0e2d5abc5e1ebcb35297cf2118a80a68b4d0
Katarzyna Janoska <k.janoska@anoriell.eu>
2026-06-30 12:42:41 +0200

    Change dialog.php to use div and proper aria for tabs

--- assets/templates/frontend/cookie/dialog.php
+++ assets/templates/frontend/cookie/dialog.php
@@ -6,7 +6,7 @@
 >
 	<div class="scm-dialog-wrapper">
 <?php include_once __DIR__ . '/header.php'; ?>
-<div class="scm-dialog-content" role="tablist">
+<div class="scm-dialog-content">
 	<div class="scm-dialog-contents">
 <?php
 $count = 0;
@@ -17,11 +17,10 @@ foreach ( $args['tabs'] as $key => $data ) {
 	}
 }
 echo '</div>';
-printf( '<ul class="scm-dialog-content-tabs scm-dialog-content-tabs-%d">', $count );
+printf( '<div class="scm-dialog-content-tabs scm-dialog-content-tabs-%d" role="tablist">', $count );
 $first = true;
 foreach ( $args['tabs'] as $key => $data ) {
 	if ( $data['show'] ) {
-		echo '<li>';
 		printf(
 			'<a data-tab="%5$s" href="#%1$s" id="%1$s-tabs-tab" class="%2$s" role="tab" aria-selected="%3$s" aria-controls="%1$s" id="%1$s-tab">%4$s</a>',
 			esc_html( $data['id'] ),
@@ -30,12 +29,11 @@ foreach ( $args['tabs'] as $key => $data ) {
 			esc_html( $data['title'] ),
 			esc_attr( $key )
 		);
-		echo '</li>';
 		$first = false;
 	}
 }
 ?>
-	</ul>
+	</div>
 </div>
 <?php include_once __DIR__ . '/footer.php'; ?>
 	</div>
